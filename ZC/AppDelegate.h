2f15421a56ec0d38579cb83b4bd817b313a16e31
guojiaming <501288952@qq.com>
2015-08-18 10:46:12 +0800

    Initial Commit

---
+++ ZC/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ZC
+//
+//  Created by 9hgame on 15/8/18.
+//  Copyright (c) 2015年 9hgame. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ZC/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ZC
+//
+//  Created by 9hgame on 15/8/18.
+//  Copyright (c) 2015年 9hgame. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
